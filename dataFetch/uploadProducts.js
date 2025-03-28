833eea8f20774b62d913a91f86efccdaf4c1776e
PantofaruTudor <tudorpantofaru@gmail.com>
2025-03-28 18:15:46 +0200

    working on data fetching and uploading

--- dataFetch/uploadProducts.js
+++ dataFetch/uploadProducts.js
@@ -1,9 +1,29 @@
-require('dotenv').config()
-const mongoose =  require('mongoose')
+//THIS IS WHERE I POPULATE THE DATABASE WITH INFO
 
+require('dotenv').config()
 const Product = require('../front&backend/models/schema')
 const connectDB = require('../front&backend/db/connect_database')
+const jsonProducts1 = require('./products.json')
+const jsonProducts2 = require('../front&backend/products.json')
 
-mongoose.connectDB(process.env.MONGO_URI)
-    .then(()=> console.log('Connected to MongoDB'))
-    .catch((err).console.error('Error connecting to MongoDB',err))
+// connectDB(process.env.MONGO_URI)
+//     .then(()=> console.log('Connected to MongoDB'))
+//     .catch((err)=> console.error('Error connecting to MongoDB',err))
+
+const populating = async()=> {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        const result = await Product.deleteMany({})
+        console.log("success")
+        await Product.create(jsonProducts)
+        console.log("succes si a doua oare")
+        return
+    }
+    catch(error)
+    {
+        console.log(error)
+        return 
+    }
+}
+
+populating()
--- front&backend/models/schema.js
+++ front&backend/models/schema.js
@@ -1,22 +1,36 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-    _id:Number,
+    //_id:Number,
     name:{
-        type:String 
+        type:String,
+        required:true
     },
     brand:{
-        type:String 
+        type:String,
+        required:true
     },
     color:{
-        type:String 
+        type:String,
+        default: "Unknown"
     },
     price:{
-        type:Number
+        type:Number,
+        required:true
     },
-    sizes: [Number],
-    stock: Number,
-    images: [String],
+    sizes: {
+        type:[Number],
+        default: []
+    },
+        
+    stock: {
+        type:Number,
+        default:0
+    },
+    images: {
+        type: [String],
+        default: []
+    },
     sale: {
         type:Boolean,
         default:false
