13bdcd044347e47e563a7bdc970758c54e416f22
PantofaruTudor <tudorpantofaru@gmail.com>
2025-05-03 15:42:24 +0300

    working on auth-endpoint errors

--- front&backend/controllers/auth_controller.js
+++ front&backend/controllers/auth_controller.js
@@ -75,6 +75,7 @@ const requestPoint = async(req,res,next)=>{
     try{
         
         //   get the token from the authorization header
+        console.log("requestPoint middleware triggered");
         const token = await req.headers.authorization.split(" ")[1];
 
         //check if the token matches the supposed origin
@@ -82,6 +83,7 @@ const requestPoint = async(req,res,next)=>{
 
         // retrieve the user details of the logged in user
         const user = await decodedToken;
+        console.log("Decoded user details:", user)
 
         // pass the user down to the endpoints here
         req.user = user;
@@ -92,7 +94,7 @@ const requestPoint = async(req,res,next)=>{
     }
     catch(error)
     {
-        response.status(401).json({error: new Error("Invalid request!")})
+        res.status(401).json({error: new Error("Invalid request!")})
     }
 }
 
